73529897a5f4eb08d7a20bd3ce95535d6f71479b
Abhijit Sengupta <Abhijit_Sengupta@neimanmarcus.com>
2018-09-13 15:50:55 +0530

    initial-project-commit

---
+++ request-discrete-distribution-service/src/main/java/com/example/request/discrete/distribution/RequestDiscreteDistributionServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.request.discrete.distribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestDiscreteDistributionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestDiscreteDistributionServiceApplication.class, args);
+	}
+}
